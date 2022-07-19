f68b3fc849301e03ba9630a0811d23ab7cfbe4e0
Zeynep <meryemzeyaltun@gmail.com>
2022-07-19 11:36:45 +0300

    proje yukleme

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndirimliFiyatHesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****** Ali Amcanın Kitapçı Dükkanı *******");
+            int x, indirim, odenecek;
+            Console.Write("Alışverişinizin toplam tutarını girin:");
+            x = Convert.ToInt32(Console.ReadLine());
+            if (x < 100)
+            {
+                indirim = x * 10 / 100;
+                odenecek = x - indirim;
+                Console.WriteLine("Ürüne yapılacak indirim:"+ indirim);
+                Console.WriteLine("Ürünün indirimli fiyatı:" + odenecek);
+            }
+            if (x >= 100 && x < 150)
+            {
+                indirim = x * 15 / 100;
+                odenecek = x - indirim;
+                Console.WriteLine("Ürüne yapılacak indirim:" + indirim);
+                Console.WriteLine("Ürünün indirimli fiyatı:" + odenecek);
+            }
+            if (x >= 150 && x < 200)
+            {
+                indirim = x * 20 / 100;
+                odenecek = x - indirim;
+                Console.WriteLine("Ürüne yapılacak indirim:" + indirim);
+                Console.WriteLine("Ürünün indirimli fiyatı:" + odenecek);
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("IndirimliFiyatHesaplama")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IndirimliFiyatHesaplama")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("7d8f2cbb-9c1d-400f-9415-4cad85d723d6")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
